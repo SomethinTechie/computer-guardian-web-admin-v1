0155463b59b150a21245a2bb3a8ed9d615afdc58
SomethinTechie <phokwanemaria@gmail.com>
2024-04-05 23:08:40 +0200

    login route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::post('/auth/login/user', [RegisterController::class, 'loginUser'])->name('login.user');
+
