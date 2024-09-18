0e4e849b079e43922da3060a4c4bbc2bae64e8e5
Mahlatse Phokwane <mahlatsephokwane@Mahlatses-Air.lan>
2024-09-18 11:25:56 +0200

    role field

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -69,6 +69,7 @@ class RegisterController extends Controller
     {
         $user = User::create([
             'name' => $data['name'],
+            'role' => 'Normal',
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
